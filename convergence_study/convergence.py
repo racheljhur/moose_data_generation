2cd8133db32e8548f8e4559e8b7532b9d5d0f314
Rachel Hur <154942153+racheljhur@users.noreply.github.com>
2025-07-10 21:13:31 -0400

    Update convergence.py

--- convergence_study/convergence.py
+++ convergence_study/convergence.py
@@ -6,16 +6,24 @@ import pandas as pd
 from classes import *
 from rich.progress import track
 
-path='/home/user/inverse_design/moose/convergence_study/mp_str_fields/FCCC50'
+# do this over the 15 classes only.
 patt='FCCC50'
-percentile=99.99
+percentile=99.99 # 99.9 is 67 per sve, 99.99 is 8 per sve, 99.999 is 2 per sve
+path=f'../../complete_workflow/combined_model/data/mp_fields/{patt}'
 
-x_dat=np.zeros((650,256,256))
+x_dat=np.zeros((650,8))
 for i in range(650):
     filename = f'arr_{i}_maxprincipal_0001.csv'
     x = extract_mp_field(path, filename)
-    x=scale(x)[0]
-    x_dat[i]=x
+    val = np.percentile(x, percentile, interpolation='lower')
+    x_top = x[x >= val]
+    x_dat[i]=x_top
+
+print(x_dat.shape)
+
+x_dat=scale(x_dat)[0] # you need to scale using the entire dataset
+mean=scale(x_dat)[1]
+std=scale(x_dat)[2]
 
 params=[]
 for i in track(drange(0,650,10)):
@@ -24,11 +32,8 @@ for i in track(drange(0,650,10)):
     else:
         x=x_dat[:i].flatten()
 
-    val = np.percentile(x, percentile, interpolation='lower')
-    x_top = x[x >= val]
-
-    p = genextreme.fit(x_top)
-    params.append((i, *p))
+    p = genextreme.fit(x)
+    params.append((i,*p))
         
     if i==649:
         num_dat=int(percentile*650*256*256)
@@ -36,14 +41,25 @@ for i in track(drange(0,650,10)):
 
 params=np.array(params)
 val = np.percentile(x_dat, percentile, interpolation='lower')
-rvs = x_dat[x_dat>=val]
+# rvs = x_dat[x_dat>=val]
+rvs=x_dat
 
-np.save(f'rvs/F30/rvs_{patt}_{percentile}.npy',rvs)
+path = f'rvs/{patt}'
+if not os.path.exists(path):
+    os.makedirs(path)
+
+np.save(f'rvs/{patt}/rvs_{patt}_{percentile}.npy',rvs)
+print('checking the shape of rvs...')
 print('rvs shape:', rvs.shape)
 
-print('checking parameter array shape and vals')
+print('checking parameter array shape and vals...')
 print(params.shape)
 print(params[:,0])
 
-print('saving...')
+path = f'params/{percentile}'
+if not os.path.exists(path):
+    os.makedirs(path)
+
 np.save(f'params/{percentile}/params_{patt}.npy', params)
+
+print(f'...finished saving the {patt} class')
