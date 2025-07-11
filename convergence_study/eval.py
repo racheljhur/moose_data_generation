7cd1ca57697a643ff27eb1a2a71f0fc0d9509ad5
Rachel Hur <154942153+racheljhur@users.noreply.github.com>
2025-07-10 21:13:43 -0400

    Update eval.py

--- convergence_study/eval.py
+++ convergence_study/eval.py
@@ -1,4 +1,4 @@
-'''evaluating statistics of fitted params'''
+import os
 import numpy as np
 from scipy.stats import genextreme
 import matplotlib.pyplot as plt
@@ -6,9 +6,13 @@ import pandas as pd
 from classes import *
 from rich.progress import track
 
-patt='F30'
+patt='FCCC50'
 percentile=99.99
-num_sve=40
+num_sve=64 # x10
+
+path=f'convergence_plots/{patt}/{percentile}'
+if not os.path.exists(path):
+    os.makedirs(path)
 
 params=np.load(f'params/{percentile}/params_{patt}.npy')
 rvs=np.load(f'rvs/{patt}/rvs_{patt}_{percentile}.npy')
@@ -16,30 +20,34 @@ rvs=np.load(f'rvs/{patt}/rvs_{patt}_{percentile}.npy')
 print(params.shape)
 print(rvs.shape)
 
-m_stats=[]
-mu_stats=[]
+mu_std=[]
+mu_mean=[]
 rel_d=[]
 for i in range(65):
     # for mean
     mean=np.mean(params[:i,1])
     std=np.std(params[:i,1])
-    mu_stats.append(mean)
-    m_stats.append(std)
+    mu_mean.append(mean)
+    mu_std.append(std)
     
-    # note: change rel_dif depending on what statistic you're assessing (mean or variance of parameter)
-    rel_dif=np.abs(mu_stats[i-1]-mu_stats[i])
+    rel_dif=np.abs(mu_std[i-1]-mu_std[i])
+    rel_dif_mean=np.abs(mu_mean[i-1]-mu_mean[i])
     rel_d.append(rel_dif)
+
+    if i==64:
+        print('absolute difference in std (across the past 100 sves):', np.abs(mu_std[64]-mu_std[54]))
+        print('absolute difference in mean: (across the past 100 sves)', np.abs(mu_mean[64]-mu_mean[54]))
     
     
-m_stats=np.array(m_stats)
-mu_stats=np.array(mu_stats)
+mu_mean=np.array(mu_mean)
+mu_std=np.array(mu_std)
 rel_d=np.array(rel_d)
-print(r'$relative difference for \mu mean:$',rel_d[num_sve]) 
+print(r'$relative difference for \mu std:$',rel_d[num_sve]) 
 
 # plot parameter progression
 n_sves=np.linspace(0,650,65)
 plt.figure()
-plt.errorbar(n_sves,params[:,1],yerr=m_stats, linewidth=2, alpha=0.35, color='blue')
+plt.errorbar(n_sves,params[:,1],yerr=mu_std, linewidth=2, alpha=0.35, color='blue')
 plt.plot(n_sves,params[:,1], linewidth=1, alpha=1)
 plt.title(f'Convergence Study at the {percentile}th Percentile \n for {patt} RVE')
 plt.grid()
@@ -47,30 +55,35 @@ plt.xlabel('Num SVEs')
 plt.ylabel(r'$\mu$')
 plt.savefig(f'convergence_plots/{patt}/{percentile}/mean.png')
 
-mu_stats=[]
-sc_stats=[]
+sc_mean=[]
+sc_std=[]
 rel_d=[]
 for i in range(65):
     # for scale
     mean=np.mean(params[:i,2])
     std=np.std(params[:i,2])
-    mu_stats.append(mean)
-    sc_stats.append(std)
+    sc_mean.append(mean)
+    sc_std.append(std)
     
 
-    rel_dif=np.abs(mu_stats[i-1]-mu_stats[i])
+    rel_dif=np.abs(sc_std[i-1]-sc_std[i])
     rel_d.append(rel_dif)
+
+    if i==64:
+        print('absolute difference in std (across the past 100 sves):', np.abs(sc_std[64]-sc_std[54]))
+        print('absolute difference in mean: (across the past 100 sves)', np.abs(sc_mean[64]-sc_mean[54]))
+
     
-mu_stats=np.array(mu_stats)
-sc_stats=np.array(sc_stats)
+sc_mean=np.array(sc_mean)
+sc_std=np.array(sc_std)
 rel_d=np.array(rel_d)
-print(r'$relative difference for \sigma mean:$',rel_d[num_sve]) 
+print(r'$relative difference for \sigma std:$',rel_d[num_sve]) 
     
 
 # plot parameter progression
 n_sves=np.linspace(0,650,65)
 plt.figure()
-plt.errorbar(n_sves,params[:,2],yerr=sc_stats, linewidth=2, alpha=0.35, color='blue')
+plt.errorbar(n_sves,params[:,2],yerr=sc_std, linewidth=2, alpha=0.35, color='blue')
 plt.plot(n_sves,params[:,2], linewidth=1, alpha=1)
 plt.title(f'Convergence Study at the {percentile}th Percentile \n for {patt} RVE')
 plt.xlabel('Num SVEs')
@@ -78,29 +91,33 @@ plt.grid()
 plt.ylabel(r'$\sigma$')
 plt.savefig(f'convergence_plots/{patt}/{percentile}/scale.png')
 
-mu_stats=[]
-k_stats=[]
+k_mean=[]
+k_std=[]
 rel_d=[]
 for i in range(65):
     # for shape
     mean=np.mean(params[:i,3])
     std=np.std(params[:i,3])
-    k_stats.append(std)
-    mu_stats.append(mean)
+    k_mean.append(mean)
+    k_std.append(std)
     
-    rel_dif=np.abs(mu_stats[i-1]-mu_stats[i])
-    rel_d.append(rel_dif)
-    
-mu_stats=np.array(mu_stats)
-k_stats=np.array(k_stats)
-rel_d=np.array(rel_d)
-print(r'$relative difference for \kappa mean:$',rel_d[num_sve]) 
 
+    rel_dif=np.abs(sc_std[i-1]-sc_std[i])
+    rel_d.append(rel_dif)
+
+    if i==64:
+        print('absolute difference in std (across the past 100 sves):', np.abs(k_std[64]-k_std[54]))
+        print('absolute difference in mean: (across the past 100 sves)', np.abs(k_mean[64]-k_mean[54]))
+    
+k_mean=np.array(k_mean)
+k_std=np.array(k_std)
+rel_d=np.array(rel_d)
+print(r'$relative difference for \kappa std:$',rel_d[num_sve]) 
 
 # plot parameter progression
 n_sves=np.linspace(0,650,65)
 plt.figure()
-plt.errorbar(n_sves,params[:,3],yerr=k_stats, linewidth=2, alpha=0.35, color='blue')
+plt.errorbar(n_sves,params[:,3],yerr=k_std, linewidth=2, alpha=0.35, color='blue')
 plt.plot(n_sves,params[:,3], linewidth=1, alpha=1)
 plt.title(f'Convergence Study at the {percentile}th Percentile \n for {patt} RVE')
 plt.xlabel('Num SVEs')
@@ -111,9 +128,9 @@ plt.savefig(f'convergence_plots/{patt}/{percentile}/shape.png')
 
 plt.figure()
 x_lin=np.linspace(rvs.min(), rvs.max(), rvs.shape[0])
-plt.hist(rvs, density=True, bins=30, alpha=0.7, edgecolor='black', label='data')
+plt.hist(rvs.flatten(), density=True, bins=30, alpha=0.7, edgecolor='black', label='data')
 # use j*10 th set of parameters in the order of shape, loc, scale
-j = 30
+j = 64
 plt.plot(x_lin,genextreme.pdf(x_lin,params[j,1],loc=params[j,2],scale=params[j,3]),'r-', lw=3, label=f'genextreme pdf')
 plt.legend()
 plt.xlabel(f'Normalized MP Stress for {patt} RVE')
@@ -121,4 +138,4 @@ plt.ylabel('Density')
 plt.title(f'Fitted GEVD for {patt} at {percentile} percentile')
 plt.savefig(f'convergence_plots/{patt}/{percentile}/hist.png')
 
-print('done')
+print(f'done evaluating the {patt} class')
