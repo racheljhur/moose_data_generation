fb3eb05a8162603552fd50c97b3e89acf45d3cdd
Rachel Hur <154942153+racheljhur@users.noreply.github.com>
2025-07-28 13:59:43 -0400

    Update convergence.py

--- convergence_study/convergence.py
+++ convergence_study/convergence.py
@@ -1,65 +1,93 @@
-''' Determining truncation threshold using Gumbel dist '''
+''' Determining truncation threshold using evds '''
+
+'''
+Based on Talreja 1981 and Rockette 1974, I chose the Gumbel Distribution (i.e., log-Weibull with kappa=0)
+for the parameteric convergence study. MLE is valid for Gumbel distributions (Rockette). This is just to 
+determine a minimum SVE ensemble size, so I can compute the responses for the remaining RVEs.
+'''
+
 import numpy as np
-from scipy.stats import genextreme
+from scipy.stats import gumbel_r
 import matplotlib.pyplot as plt
 import pandas as pd
 from helpers import *
 from rich.progress import track
+import json
+import pickle
 
-# do this over the 15 classes only.
-patt='FCCC50'
-percentile=99.99 # 99.9 is 67 per sve, 99.99 is 8 per sve, 99.999 is 2 per sve
-path=f'../../complete_workflow/combined_model/data/mp_fields/{patt}'
+# Do this over the 15 classes only.
+patterns=['F30','FA30', 'FAAA30', 'FC30', 'FCCC30', 'F40', 'FA40', 'FAAA40', 'FC40', 'FCCC40',
+          'F50', 'FA50', 'FAAA50', 'FC50', 'FCCC50']
+percentile=99 # This will correspond to 56 values / SVE
+res=256
+dat_dir=f'../../complete_workflow/combined_model/data/mp_fields'
 
-x_dat=np.zeros((650,8))
-for i in range(650):
-    filename = f'arr_{i}_maxprincipal_0001.csv'
-    x = extract_mp_field(path, filename)
-    val = np.percentile(x, percentile, interpolation='lower')
-    x_top = x[x >= val]
-    x_dat[i]=x_top
-
-print(x_dat.shape)
-
-x_dat=scale(x_dat)[0] # you need to scale using the entire dataset
-mean=scale(x_dat)[1]
-std=scale(x_dat)[2]
-
-params=[]
-for i in track(drange(0,650,10)):
-    if i==0:
-        x=x_dat[i].flatten()
-    else:
-        x=x_dat[:i].flatten()
-
-    p = genextreme.fit(x)
-    params.append((i,*p))
+# Use a dictionary to access values with patterns
+x_dat = {}
+for patt in patterns:
+    x_dat[patt] = []
+    for i in track(range(650)):
         
-    if i==649:
-        num_dat=int(percentile*650*256*256)
-        print(f'final shape of dataset used for mle (should be ({num_dat},)):',x_top.shape)
+        path=dat_dir + f'/{patt}'
+        filename = f'arr_{i}_maxprincipal_0001.csv'
+        x = extract_mp_field(path, filename, res)
+        
+        # You need to ensure that you're sampling the top 1% from the fixed physical domain determined from the prior study
+        x = resample_to_75(x)
+        val = np.percentile(x, percentile, interpolation='nearest')
+        x_top = x[x > val]
+        x_dat[patt].append(x_top)
 
-params=np.array(params)
-val = np.percentile(x_dat, percentile, interpolation='lower')
-# rvs = x_dat[x_dat>=val]
-rvs=x_dat
+    x_dat[patt]=np.array(x_dat[patt])
+    print(x_dat[patt].shape)
+print(f'finished loading mp stresses at resolution {res}')
 
-path = f'rvs/{patt}'
+x_dat_scaled={}
+# Scale the datasets
+for patt in patterns:
+    x_dat_scaled[patt]=scale(x_dat[patt])[0]
+    mean=scale(x_dat[patt])[1]
+    std=scale(x_dat[patt])[2]
+    print(mean)
+    print(std)
+    np.save(f'rvs/{res}/dat_mean_{patt}.npy',mean)
+    np.save(f'rvs/{res}/dat_std_{patt}.npy',std)
+    print(f'saved mean and std of x_dat for {patt}')
+
+# fit gumbel distribution (i.e., fit log weibull loc and scale, keeping kappa=0)
+params={}
+for patt in patterns:
+    params[patt]=[]
+    for i in track(drange(0,650,10)):
+        if i==0:
+            x=x_dat[patt][i].flatten()
+        else:
+            x=x_dat[patt][:i].flatten()
+
+        p = gumbel_r.fit(x)
+        params[patt].append((i,*p))
+            
+        if i==649:
+            num_dat=int(percentile*650*256*256)
+            print(f'final shape of dataset used for mle (should be ({num_dat},)):',x_top.shape)
+
+# write results
+print('saving params and rvs...')
+
+path = f'params/{res}'
+if not os.path.exists(path):
+    os.makedirs(path)
+path = f'rvs/{res}'
 if not os.path.exists(path):
     os.makedirs(path)
 
-np.save(f'rvs/{patt}/rvs_{patt}_{percentile}.npy',rvs)
-print('checking the shape of rvs...')
-print('rvs shape:', rvs.shape)
+with open(f'params/{res}/params.pkl', 'wb') as f:
+    pickle.dump(params, f)
 
-print('checking parameter array shape and vals...')
-print(params.shape)
-print(params[:,0])
+with open(f'rvs/{res}/rvs_scaled.pkl', 'wb') as f:
+    pickle.dump(x_dat_scaled, f)
 
-path = f'params/{percentile}'
-if not os.path.exists(path):
-    os.makedirs(path)
+with open(f'rvs/{res}/rvs.pkl', 'wb') as f:
+    pickle.dump(x_dat, f)
 
-np.save(f'params/{percentile}/params_{patt}.npy', params)
-
-print(f'...finished saving the {patt} class')
+print(f'Done for resolution {res}')
