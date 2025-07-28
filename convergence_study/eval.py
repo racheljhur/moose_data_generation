820228042a2cbc21cac3d2b82a7c72e4f1ff9cad
Rachel Hur <154942153+racheljhur@users.noreply.github.com>
2025-07-28 14:00:14 -0400

    Update eval.py

--- convergence_study/eval.py
+++ convergence_study/eval.py
@@ -1,141 +1,124 @@
+''' Plotting Functions for Evaluating Fitted Gumbel Parameters'''
 import os
 import numpy as np
-from scipy.stats import genextreme
+from scipy.stats import gumbel_r
 import matplotlib.pyplot as plt
 import pandas as pd
 from helpers import *
 from rich.progress import track
+import json
+import pickle
 
-patt='FCCC50'
-percentile=99.99
-num_sve=64 # x10
+# Global Variables
+patterns=['F30','FA30', 'FAAA30', 'FC30', 'FCCC30', 'F40', 'FA40', 'FAAA40', 'FC40', 'FCCC40',
+          'F50', 'FA50', 'FAAA50', 'FC50', 'FCCC50']
+res=256
+percentile=99
 
-path=f'convergence_plots/{patt}/{percentile}'
+path=f'convergence_plots/{res}/{percentile}'
 if not os.path.exists(path):
     os.makedirs(path)
 
-params=np.load(f'params/{percentile}/params_{patt}.npy')
-rvs=np.load(f'rvs/{patt}/rvs_{patt}_{percentile}.npy')
+# load in dictionaries, where entries are accessible via pattern name
+with open(f'params/{res}/params.pkl', 'rb') as f:
+    params = pickle.load(f)
+with open(f'rvs/{res}/rvs.pkl', 'rb') as f:
+    rvs = pickle.load(f)
 
-print(params.shape)
-print(rvs.shape)
-
-mu_std=[]
-mu_mean=[]
-rel_d=[]
-for i in range(65):
-    # for mean
-    mean=np.mean(params[:i,1])
-    std=np.std(params[:i,1])
-    mu_mean.append(mean)
-    mu_std.append(std)
+# grab the statistics of the location parameter (mean and std)
+mu_std={}
+mu_mean={}
+# loc parameter is stored in column 2, i.e., params[n,1]
+for patt in patterns:
+    mu_mean[patt]=[]
+    mu_std[patt]=[]
+    for i in drange(0,65,15): 
+        params[patt]=np.array(params[patt])
+        mean = np.mean(params[patt][:i, 1])
+        std = np.std(params[patt][:i, 1])
+        mu_mean[patt].append(mean)
+        mu_std[patt].append(std)
     
-    rel_dif=np.abs(mu_std[i-1]-mu_std[i])
-    rel_dif_mean=np.abs(mu_mean[i-1]-mu_mean[i])
-    rel_d.append(rel_dif)
+    mu_mean[patt]=np.array(mu_mean[patt])
+    mu_std[patt]=np.array(mu_std[patt])
 
-    if i==64:
-        print('absolute difference in std (across the past 100 sves):', np.abs(mu_std[64]-mu_std[54]))
-        print('absolute difference in mean: (across the past 100 sves)', np.abs(mu_mean[64]-mu_mean[54]))
-    
-    
-mu_mean=np.array(mu_mean)
-mu_std=np.array(mu_std)
-rel_d=np.array(rel_d)
-print(r'$relative difference for \mu std:$',rel_d[num_sve]) 
-
-# plot parameter progression
+# plot parameter progression for the location parameter 
 n_sves=np.linspace(0,650,65)
-plt.figure()
-plt.errorbar(n_sves,params[:,1],yerr=mu_std, linewidth=2, alpha=0.35, color='blue')
-plt.plot(n_sves,params[:,1], linewidth=1, alpha=1)
-plt.title(f'Convergence Study at the {percentile}th Percentile \n for {patt} RVE')
-plt.grid()
-plt.xlabel('Num SVEs')
-plt.ylabel(r'$\mu$')
-plt.savefig(f'convergence_plots/{patt}/{percentile}/mean.png')
+for patt in patterns:
+    # access the (5,) shaped mu_std array for each pattern
+    # access the (65,) shaped location parameter in patt[:,1] for each pattern
+    plt.figure()
+    # plt.errorbar(n_sves,params[patt][:,1],yerr=mu_std[patt], linewidth=2, alpha=0.35, color='blue')
+    plt.plot(n_sves,params[patt][:,1], linewidth=2, alpha=1)
+    plt.title(f'Convergence Study at the {percentile}th Percentile \n for {patt} RVE')
+    plt.grid()
+    plt.xlabel('Num SVEs')
+    plt.ylabel(r'$\mu$')
+    plt.savefig(f'convergence_plots/{res}/{percentile}/loc_{patt}.png')
 
-sc_mean=[]
-sc_std=[]
-rel_d=[]
-for i in range(65):
-    # for scale
-    mean=np.mean(params[:i,2])
-    std=np.std(params[:i,2])
-    sc_mean.append(mean)
-    sc_std.append(std)
+
+# grab the statistics of the location parameter (mean and std)
+sc_mean={}
+sc_std={}
+for patt in patterns:
+    sc_mean[patt]=[]
+    sc_std[patt]=[]
+    for i in drange(0,65,15):
+        params[patt]=np.array(params[patt])   
+        # compute the mean and std at increments of 150       
+        mean=np.mean(params[patt][:i,2])
+        std=np.std(params[patt][:i,2])
+        sc_mean[patt].append(mean)
+        sc_std[patt].append(std)
     
-
-    rel_dif=np.abs(sc_std[i-1]-sc_std[i])
-    rel_d.append(rel_dif)
-
-    if i==64:
-        print('absolute difference in std (across the past 100 sves):', np.abs(sc_std[64]-sc_std[54]))
-        print('absolute difference in mean: (across the past 100 sves)', np.abs(sc_mean[64]-sc_mean[54]))
-
+    sc_mean[patt]=np.array(sc_mean[patt])
+    sc_std[patt]=np.array(sc_std[patt])
     
-sc_mean=np.array(sc_mean)
-sc_std=np.array(sc_std)
-rel_d=np.array(rel_d)
-print(r'$relative difference for \sigma std:$',rel_d[num_sve]) 
-    
+# plot parameter progression for the scale parameter
+for patt in patterns:
+    # access the (5,) shaped mu_std array for each pattern
+    # access the (65,) shaped scale parameter in patt[:,1] for each pattern
+    plt.figure()
+    # plt.errorbar(n_sves,params[patt][:,2],yerr=sc_std[patt], linewidth=2, alpha=0.35, color='blue')
+    plt.plot(n_sves,params[patt][:,2], linewidth=2, alpha=1)
+    plt.title(f'Convergence Study at the {percentile}th Percentile \n for {patt} RVE')
+    plt.grid()
+    plt.xlabel('Num SVEs')
+    plt.ylabel(r'$\sigma$')
+    plt.savefig(f'convergence_plots/{res}/{percentile}/scale_{patt}.png')
 
-# plot parameter progression
-n_sves=np.linspace(0,650,65)
-plt.figure()
-plt.errorbar(n_sves,params[:,2],yerr=sc_std, linewidth=2, alpha=0.35, color='blue')
-plt.plot(n_sves,params[:,2], linewidth=1, alpha=1)
-plt.title(f'Convergence Study at the {percentile}th Percentile \n for {patt} RVE')
-plt.xlabel('Num SVEs')
-plt.grid()
-plt.ylabel(r'$\sigma$')
-plt.savefig(f'convergence_plots/{patt}/{percentile}/scale.png')
+# plotting the fitted Gumbel distribution
+for patt in patterns:
+    # use unnormalized variables
+    x_dat=rvs[patt]
+    # use j*10 th set of parameters (likely has converged)
+    j=64
 
-k_mean=[]
-k_std=[]
-rel_d=[]
-for i in range(65):
-    # for shape
-    mean=np.mean(params[:i,3])
-    std=np.std(params[:i,3])
-    k_mean.append(mean)
-    k_std.append(std)
-    
-
-    rel_dif=np.abs(sc_std[i-1]-sc_std[i])
-    rel_d.append(rel_dif)
-
-    if i==64:
-        print('absolute difference in std (across the past 100 sves):', np.abs(k_std[64]-k_std[54]))
-        print('absolute difference in mean: (across the past 100 sves)', np.abs(k_mean[64]-k_mean[54]))
-    
-k_mean=np.array(k_mean)
-k_std=np.array(k_std)
-rel_d=np.array(rel_d)
-print(r'$relative difference for \kappa std:$',rel_d[num_sve]) 
-
-# plot parameter progression
-n_sves=np.linspace(0,650,65)
-plt.figure()
-plt.errorbar(n_sves,params[:,3],yerr=k_std, linewidth=2, alpha=0.35, color='blue')
-plt.plot(n_sves,params[:,3], linewidth=1, alpha=1)
-plt.title(f'Convergence Study at the {percentile}th Percentile \n for {patt} RVE')
-plt.xlabel('Num SVEs')
-plt.grid()
-plt.ylabel(r'$\kappa$')
-plt.savefig(f'convergence_plots/{patt}/{percentile}/shape.png')
+    plt.figure()
+    x_lin=np.linspace(x_dat.min(), x_dat.max(), x_dat.shape[0])
+    plt.hist(x_dat.flatten(), density=True, bins=30, alpha=0.7, edgecolor='black', label='data')
+    plt.plot(x_lin,gumbel_r.pdf(x_lin,loc=params[patt][j,1],scale=params[patt][j,2]),'r-', lw=3, label=f'Gumbel pdf')
+    plt.legend()
+    plt.xlabel(f'MP Stress for {patt} RVE')
+    plt.ylabel('Density')
+    plt.title(f'Gumbel Distribution for {patt} at {percentile} percentile')
+    plt.savefig(f'convergence_plots/{res}/{percentile}/hist_{patt}_unnormalized.png')
 
 
-plt.figure()
-x_lin=np.linspace(rvs.min(), rvs.max(), rvs.shape[0])
-plt.hist(rvs.flatten(), density=True, bins=30, alpha=0.7, edgecolor='black', label='data')
-# use j*10 th set of parameters in the order of shape, loc, scale
-j = 64
-plt.plot(x_lin,genextreme.pdf(x_lin,params[j,1],loc=params[j,2],scale=params[j,3]),'r-', lw=3, label=f'genextreme pdf')
-plt.legend()
-plt.xlabel(f'Normalized MP Stress for {patt} RVE')
-plt.ylabel('Density')
-plt.title(f'Fitted GEVD for {patt} at {percentile} percentile')
-plt.savefig(f'convergence_plots/{patt}/{percentile}/hist.png')
+# check the relative change from 450 to 650
+for patt in patterns:
+    # print(f'location statistics for {patt} (percent change)')
+    # rel_diff_mean=np.abs(mu_mean[patt][2]-mu_mean[patt][4]) / mu_mean[patt][2]
+    # rel_diff_std=np.abs(mu_std[patt][2]-mu_std[patt][4]) / mu_std[patt][2]
 
-print(f'done evaluating the {patt} class')
+    # print(f"{rel_diff_mean:.2e}")
+    # print(f"{rel_diff_std:.2e}")
+
+    print(f'scale statistics for {patt} (percent change)')
+    # rel_diff_mean=np.abs(sc_mean[patt][2]-sc_mean[patt][4]) / sc_mean[patt][2]
+    rel_diff_std=np.abs(sc_std[patt][2]-sc_std[patt][4]) / sc_std[patt][2]
+
+    # print(f"{rel_diff_mean:.2e}")
+    print(f"{rel_diff_std:.2e}")
+
+print(f'finished evaluating all classes at resolution {res}')
